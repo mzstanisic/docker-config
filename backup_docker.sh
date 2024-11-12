390672a9656c2ca11eb41773930c2502cf86f87b
mzstanisic <94664112+mzstanisic@users.noreply.github.com>
2024-11-11 20:09:38 -0500

    Update backup_docker.sh

--- backup_docker.sh
+++ backup_docker.sh
@@ -9,10 +9,23 @@
 ##########################################################################################
 
 # Docker config folders to backup.
-backup_files="$HOME/docker"
+backup_files="/mnt/docker"
 
 # Docker volumes to backup.
-volumes=("joplin-server_joplin-db-data" "hoarder_data" "hoarder_meilisearch" "portainer_data")
+volumes=(
+	"joplin-server_data"
+	"hoarder_data"
+	"hoarder_meilisearch"
+	"portainer_data"
+	"code-server_config"
+	"jellyfin_config"
+	"jellyfin_cache"
+	"media-management_bazarr"
+	"media-management_prowlarr"
+	"media-management_radarr"
+	"media-management_sonarr"
+	"media-management_qbittorrent"
+	)
 
 # Where to backup to.
 dest="/mnt/backup/backups/docker"
@@ -24,31 +37,31 @@ hostname=$(hostname -s)
 # Find which week of the month 1-4 it is.
 day_num=$(date +%-d)
 if (( $day_num <= 7)); then
-        week_file="$hostname-week1.tgz"
+	week_file="$hostname-week1.tgz"
 elif (( $day_num > 7 && $day_num <= 14 )); then
-        week_file="$hostname-week2.tgz"
+	week_file="$hostname-week2.tgz"
 elif (( $day_num > 14 && $day_num <= 21 )); then
-        week_file="$hostname-week3.tgz"
+	week_file="$hostname-week3.tgz"
 elif (( $day_num > 21 && $day_num < 32 )); then
-        week_file="$hostname-week4.tgz"
+	week_file="$hostname-week4.tgz"
 fi
 
 # Find if the Month is odd or even.
 month_num=$(date +%m)
 month=$(expr $month_num % 2)
 if [ $month -eq 0 ]; then
-        month_file="$hostname-month2.tgz"
+	month_file="$hostname-month2.tgz"
 else
-        month_file="$hostname-month1.tgz"
+	month_file="$hostname-month1.tgz"
 fi
 
 # Create archive filename.
 if [ $day_num == 1 ]; then
-        archive_file=$month_file
+	archive_file=$month_file
 elif [ $day != "Saturday"  ]; then
-        archive_file="$hostname-$day.tgz"
+	archive_file="$hostname-$day.tgz"
 else
-        archive_file=$week_file
+	archive_file=$week_file
 fi
 
 # Print start status message.
